0fe4c7718dd808ff02fd858efc142ad6956768c6
vitalfocheux <vital.focheux@edu.univ-fcomte.fr>
2024-09-18 14:20:40 +0200

    Ajout des méthodes de la classe Node

--- src/main/m1graphs2024/Node.java
+++ src/main/m1graphs2024/Node.java
@@ -1,5 +1,7 @@
 package main.m1graphs2024;
 
+import java.util.*;
+
 public class Node implements Comparable<Node>{
 
     private int id;
@@ -18,5 +20,57 @@ public class Node implements Comparable<Node>{
     @Override
     public int compareTo(Node o) {
         return 0;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getGraph() {
+        return 0;
+    }
+
+    public int getName() {
+        return 0;
+    }
+
+    public List<Node> getSuccessors() {
+        return null;
+    }
+
+    public List<Node> getSuccessorsMulti(){
+        return null;
+    }
+
+    public boolean adjacent(Node n){
+        return false;
+    }
+
+    public int inDegree(Node n){
+        return 0;
+    }
+
+    public int outDegree(Node n){
+        return 0;
+    }
+
+    public int degree(Node n){
+        return 0;
+    }
+
+    public List<Edge> getOutEdges(){
+        return null;
+    }
+
+    public List<Edge> getInEdges(){
+        return null;
+    }
+
+    public List<Edge> getIncidentEdges(){
+        return null;
+    }
+
+    public List<Edge> getEdgesTo(Node n){
+        return null;
     }
 }
