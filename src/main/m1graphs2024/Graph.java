7ab1c6a727f6337fbc3b87071f831581e93a1849
vitalfocheux <vital.focheux@edu.univ-fcomte.fr>
2024-09-18 14:16:32 +0200

    Ajout des TODO & Constructeurs

--- src/main/m1graphs2024/Graph.java
+++ src/main/m1graphs2024/Graph.java
@@ -1,6 +1,20 @@
 package main.m1graphs2024;
 
+import java.util.*;
+
 public class Graph {
 
     private Map<Node, List<Edge>> adjEdList;
+
+    public Graph(){
+        adjEdList = new HashMap<>();
+    }
+
+    public Graph(int ... nodes){
+        //TODO Constructeur decrit avec le Successor Array
+        adjEdList = new HashMap<>();
+
+    }
+
+    //TODO Faire un constructeur qui lit un fichier .dot
 }
