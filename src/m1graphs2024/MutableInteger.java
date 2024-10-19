0d3abe159edeb4c6f21e4c9a537a9e322f293e12
vitalfocheux <vital.focheux@edu.univ-fcomte.fr>
2024-10-19 19:49:43 +0200

    Ajout d'une permettant de rendre un entier mutable

---
+++ src/m1graphs2024/MutableInteger.java
@@ -0,0 +1,22 @@
+package m1graphs2024;
+
+public class MutableInteger {
+
+    private int value;
+
+    public MutableInteger(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public void increment() {
+        value++;
+    }
+}
